2e86d97c94977228b0c24baaa920c939e0443e69
arunreddy096 <arunreddy096@gmail.com>
2023-03-21 22:10:33 -0400

    order history, booking succesfull view and html pages

--- booking_jumpstart/jumpstart/urls.py
+++ booking_jumpstart/jumpstart/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-from .views import CreateBookingView, OrderHistoryView
+from .views import CreateBookingView, OrderHistoryView, BookingSuccessView
 
 urlpatterns = [
     path('login/', views.LoginSignup.as_view(), name='login'),
@@ -8,5 +8,7 @@ urlpatterns = [
     path('forgot_password/', views.ForgotPassword.as_view(), name='forgot'),
     path('order_history/', views.OrderHistoryView.as_view(), name='order_history'),
     path('booking/create/', CreateBookingView.as_view(), name='create_booking'),
+    path('booking/success/<int:booking_id>/', BookingSuccessView.as_view(), name='booking_success'),
+
 
 ]
